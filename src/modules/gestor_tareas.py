1c65e41dafd04b637d333217d31e0cdb9c39f8f8
HectorAyusoMartin <hector.ayuso.martin@gmail.com>
2024-12-11 13:12:46 +0100

    añadida persistencia usando un JSON

--- src/modules/gestor_tareas.py
+++ src/modules/gestor_tareas.py
@@ -1,9 +1,23 @@
+import json
+import os
+
+
+
 
 class GestorTareas:
-    def __init__(self):
-        #vamos a inicializar una lista de tareas..
+    def __init__(self, archivo ='tareas.json'):
+             
+        #iniciamos la lista de tareas de carga desde un archivo
+        
+        # Asegura que el archivo JSON esté en la misma carpeta que este script
+        self.archivo = os.path.join(os.path.dirname(__file__), archivo)
+        
         
         self.lista_tareas = []
+        
+        
+        self.cargar_tareas()
+        
         
         
     def agregar_tarea(self, descripcion, prioridad):
@@ -16,7 +30,7 @@ class GestorTareas:
         
         self.lista_tareas.sort(key=lambda t: {"Alta": 1, "Media": 2, "Baja": 3}[t["prioridad"]])
 
-        
+        self.guardar_tareas()
         
     def listar_tareas(self):
         
@@ -27,10 +41,45 @@ class GestorTareas:
     
     def eliminar_tarea(self, indice):
         
+        
         if 0 <= indice < len(self.lista_tareas):
             self.lista_tareas.pop(indice)
+            self.guardar_tareas()
             
         else:
             raise IndexError("Indice de la tarea no valido")
         
+
+
+    def cargar_tareas(self):
+        if os.path.exists(self.archivo):
+            try:
+                with open(self.archivo, 'r') as f:
+                    self.lista_tareas = json.load(f)
+                    print(f'Tareas cargadas desde {self.archivo}')
+            except FileNotFoundError:
+                self.lista_tareas = [] #si no existe, inicia lista vacia
+            except json.JSONDecodeError:
+                print(f'Error al leer el archivo {self.archivo}, inicializando lista vacia')
+                self.lista_tareas = []
+        else:
+            print(f'No se encontro {self.archivo},inicializando lista vacia')
+            self.lista_tareas = []
+                
+            
+            
+            
+    
+    
+    def guardar_tareas(self):
         
+        try:
+            
+            with open(self.archivo, 'w') as f:
+                json.dump(self.lista_tareas ,f, indent=4)
+            print(f'Tareas guardadas en {self.archivo}')
+        
+        except Exception as e:
+            print(f'Error al guardar las tareas: {e}')
+        
+    
