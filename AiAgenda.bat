e2616b57161ac7f9c7bcebf7f2511d9915365ab7
HectorAyusoMartin <hector.ayuso.martin@gmail.com>
2024-12-12 18:10:43 +0100

    añadiendo ejecutables bat para facil inicio de la app

---
+++ AiAgenda.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+cd src
+
+streamlit run ai_agenda_app.py
+
--- src/ejecutable.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Héctor\OneDrive\Escritorio\AI AGENDA RECUPERADA\src\main.py"
-python main.py
-pause
